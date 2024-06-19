eb9282bf35cf8d05a881c63b62e217d815852253
sohel <sohelshaikh01231@gmail.com>
2024-06-19 11:54:45 +0530

    error fixed

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,19 +1,10 @@
-import Project from "@/components/Project";
-import { ProjectTypes } from "@/types/types";
-import { getProjects } from "@/utils/getProjects";
-import { sql } from "@vercel/postgres";
+import Projects from "@/components/Projects";
 
 export default async function page() {
-  const projects = await getProjects();
-  console.log(projects);
   return (
     <section className="max-w-screen-lg mx-auto my-12 p-5">
       <h1 className="text-3xl font-bold">Projects.</h1>
-      <div className="grid grid-cols-2 mt-7 gap-12">
-        {projects?.map((project) => (
-          <Project data={project} />
-        ))}
-      </div>
+      <Projects />
     </section>
   );
 }
